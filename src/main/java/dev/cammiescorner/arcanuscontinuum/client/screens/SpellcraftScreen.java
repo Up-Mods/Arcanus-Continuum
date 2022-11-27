9ff6919b22312d6ee9e92925da22528294f0de29
CammiePone <inazuma261@gmail.com>
2022-11-27 01:45:17 -0500

    brains no work, braincells on holiday

--- src/main/java/dev/cammiescorner/arcanuscontinuum/client/screens/SpellcraftScreen.java
+++ src/main/java/dev/cammiescorner/arcanuscontinuum/client/screens/SpellcraftScreen.java
@@ -1,10 +1,16 @@
 package dev.cammiescorner.arcanuscontinuum.client.screens;
 
 import dev.cammiescorner.arcanuscontinuum.common.screens.SpellcraftScreenHandler;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.font.TextRenderer;
 import net.minecraft.client.gui.screen.ingame.HandledScreen;
+import net.minecraft.client.gui.widget.ClickableWidget;
+import net.minecraft.client.render.item.ItemRenderer;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.text.Text;
+
+import java.util.List;
 
 public class SpellcraftScreen extends HandledScreen<SpellcraftScreenHandler> {
 	public SpellcraftScreen(SpellcraftScreenHandler screenHandler, PlayerInventory playerInventory, Text text) {
@@ -14,5 +20,25 @@ public class SpellcraftScreen extends HandledScreen<SpellcraftScreenHandler> {
 	@Override
 	protected void drawBackground(MatrixStack matrices, float delta, int mouseX, int mouseY) {
 
+	}
+
+	@Override
+	public List<ClickableWidget> getButtons() {
+		return null;
+	}
+
+	@Override
+	public ItemRenderer getItemRenderer() {
+		return getClient().getItemRenderer();
+	}
+
+	@Override
+	public TextRenderer getTextRenderer() {
+		return getClient().textRenderer;
+	}
+
+	@Override
+	public MinecraftClient getClient() {
+		return MinecraftClient.getInstance();
 	}
 }
--- src/main/java/dev/cammiescorner/arcanuscontinuum/common/entities/OpossumEntity.java
+++ src/main/java/dev/cammiescorner/arcanuscontinuum/common/entities/OpossumEntity.java
@@ -2,12 +2,16 @@ package dev.cammiescorner.arcanuscontinuum.common.entities;
 
 import dev.cammiescorner.arcanuscontinuum.common.registry.ArcanusEntities;
 import dev.cammiescorner.arcanuscontinuum.common.registry.ArcanusItems;
+import it.unimi.dsi.fastutil.objects.ObjectArrayList;
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.ai.brain.Brain;
 import net.minecraft.entity.attribute.DefaultAttributeContainer;
 import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.data.DataTracker;
 import net.minecraft.entity.data.TrackedData;
 import net.minecraft.entity.data.TrackedDataHandlerRegistry;
+import net.minecraft.entity.effect.StatusEffect;
+import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.passive.PassiveEntity;
 import net.minecraft.entity.passive.TameableEntity;
 import net.minecraft.entity.player.PlayerEntity;
@@ -17,11 +21,19 @@ import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.world.World;
+import net.tslat.smartbrainlib.api.SmartBrainOwner;
+import net.tslat.smartbrainlib.api.core.SmartBrainProvider;
+import net.tslat.smartbrainlib.api.core.sensor.ExtendedSensor;
+import net.tslat.smartbrainlib.api.core.sensor.vanilla.HurtBySensor;
+import net.tslat.smartbrainlib.api.core.sensor.vanilla.NearbyLivingEntitySensor;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+import org.quiltmc.qsl.entity.effect.api.StatusEffectRemovalReason;
 
+import java.util.List;
 import java.util.UUID;
 
-public class OpossumEntity extends TameableEntity {
+public class OpossumEntity extends TameableEntity implements SmartBrainOwner<OpossumEntity> {
 	private static final TrackedData<ItemStack> HAT_TRACKER = DataTracker.registerData(OpossumEntity.class, TrackedDataHandlerRegistry.ITEM_STACK);
 
 	public OpossumEntity(EntityType<? extends TameableEntity> entityType, World world) {
@@ -91,11 +103,45 @@ public class OpossumEntity extends TameableEntity {
 		return super.writeNbt(nbt);
 	}
 
+	@Override
+	public boolean removeStatusEffect(@NotNull StatusEffect type, @NotNull StatusEffectRemovalReason reason) {
+		return false;
+	}
+
+	@Override
+	public int clearStatusEffects(@NotNull StatusEffectRemovalReason reason) {
+		return 0;
+	}
+
+	@Override
+	public void onStatusEffectRemoved(@NotNull StatusEffectInstance effect, @NotNull StatusEffectRemovalReason reason) {
+
+	}
+
 	public ItemStack getHatStack() {
 		return dataTracker.get(HAT_TRACKER);
 	}
 
 	public void setHatStack(ItemStack stack) {
 		dataTracker.set(HAT_TRACKER, stack);
+	}
+
+	@Override
+	protected Brain.Profile<?> createBrainProfile() {
+		return new SmartBrainProvider<>(this);
+	}
+
+	@Override
+	protected void mobTick() {
+		tickBrain(this);
+		super.mobTick();
+	}
+
+	@Override
+	public List<ExtendedSensor<OpossumEntity>> getSensors() {
+		return ObjectArrayList.of(
+				new NearbyLivingEntitySensor<>(),
+				new HurtBySensor<>()
+		);
 	}
 }
