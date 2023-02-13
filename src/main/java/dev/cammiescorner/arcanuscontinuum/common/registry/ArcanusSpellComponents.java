8a973f2ef0ca2ab5979983108279841e2c3ecff3
CammiePone <inazuma261@gmail.com>
2023-02-13 00:35:07 -0500

    some structure shenanigans, need a processor

--- src/main/java/dev/cammiescorner/arcanuscontinuum/common/registry/ArcanusSpellComponents.java
+++ src/main/java/dev/cammiescorner/arcanuscontinuum/common/registry/ArcanusSpellComponents.java
@@ -25,7 +25,7 @@ public class ArcanusSpellComponents {
 	public static final SpellShape BOLT = create("bolt_shape", new BoltSpellShape(Weight.VERY_LIGHT, 0.5, 5, 1));
 	public static final SpellShape BEAM = create("beam_shape", new BeamSpellShape(Weight.VERY_LIGHT, 0.5, 5, 1)); // TODO charge + lock-on
 	public static final SpellShape RUNE = create("rune_shape", new RuneSpellShape(Weight.VERY_LIGHT, 0.5, 5, 1)); // TODO entity
-	public static final SpellShape SMITE = create("smite_shape", new SmiteSpellShape(Weight.VERY_LIGHT, 0.5, 5, 1)); // TODO entity
+	public static final SpellShape SMITE = create("smite_shape", new SmiteSpellShape(Weight.VERY_LIGHT, 0.5, 5, 1));
 	public static final SpellShape AOE = create("aoe_shape", new TouchSpellShape(Weight.VERY_LIGHT, 0.5, 5, 1)); // TODO entity
 	public static final SpellShape EXPLOSION = create("explosion_shape", new ExplosionSpellShape(Weight.VERY_LIGHT, 0.5, 5, 1));
 
