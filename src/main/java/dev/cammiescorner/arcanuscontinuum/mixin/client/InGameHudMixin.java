2ec404f9e4f26ab659fed5a7a107017cf71890ee
Abigail Ient <abigail@ient.me>
2023-08-23 13:23:50 +0100

    fix mixin signature

--- src/main/java/dev/cammiescorner/arcanuscontinuum/mixin/client/InGameHudMixin.java
+++ src/main/java/dev/cammiescorner/arcanuscontinuum/mixin/client/InGameHudMixin.java
@@ -5,6 +5,7 @@ import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.hud.InGameHud;
 import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -15,11 +16,11 @@ public class InGameHudMixin {
 	@Shadow private int scaledHeight;
 
 	@WrapOperation(method = "renderCrosshair", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiGraphics;drawTexture(Lnet/minecraft/util/Identifier;IIIIII)V", ordinal = 0))
-	private void arcanuscontinuum$moveCrosshair(GuiGraphics gui, int x, int y, int u, int v, int w, int h, Operation<Void> original) {
-		MatrixStack matrices = gui.getMatrices();
+	private void arcanuscontinuum$moveCrosshair(GuiGraphics instance, Identifier id, int x, int y, int u, int v, int w, int h, Operation<Void> original) {
+		MatrixStack matrices = instance.getMatrices();
 		matrices.push();
 		matrices.translate((scaledWidth - 15) / 2F, (scaledHeight - 15) / 2F, 0);
-		original.call(matrices, 0, 0, u, v, w, h);
+		original.call(instance, id, 0, 0, u, v, w, h);
 		matrices.pop();
 	}
 }
