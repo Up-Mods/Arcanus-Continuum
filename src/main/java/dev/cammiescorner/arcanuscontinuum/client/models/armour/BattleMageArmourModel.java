f0235a2697774b1ceb599b20055b7c5f7be42e31
CammiePone <inazuma261@gmail.com>
2024-01-16 07:42:15 -0800

    removed/merged about 30% of the armour's cubes

--- src/main/java/dev/cammiescorner/arcanuscontinuum/client/models/armour/BattleMageArmourModel.java
+++ src/main/java/dev/cammiescorner/arcanuscontinuum/client/models/armour/BattleMageArmourModel.java
@@ -46,35 +46,27 @@ public class BattleMageArmourModel<T extends LivingEntity> extends BipedEntityMo
 		ModelPartData rightLeg = root.getChild(EntityModelPartNames.RIGHT_LEG);
 		ModelPartData leftLeg = root.getChild(EntityModelPartNames.LEFT_LEG);
 
-		ModelPartData armorHead = head.addChild("armorHead", ModelPartBuilder.create().uv(0, 144).cuboid(-4f, -8f, -4f, 8f, 8f, 8f, new Dilation(0.53f))
-		.uv(18, 223).cuboid(0f, -17f, -7.5f, 0f, 15f, 18f, new Dilation(0f))
-		.uv(28, 130).cuboid(-1f, -10f, -6f, 2f, 2f, 12f, new Dilation(0f))
-		.uv(52, 128).cuboid(-1f, -8f, -6f, 2f, 6f, 2f, new Dilation(0f))
-		.uv(44, 128).cuboid(-1f, -8f, 4f, 2f, 8f, 2f, new Dilation(0f))
-		.uv(32, 144).cuboid(-4f, -8f, -4f, 8f, 8f, 8f, new Dilation(0.55f)), ModelTransform.pivot(0f, 0f, 0f));
+		ModelPartData armorHead = head.addChild("armorHead", ModelPartBuilder.create().uv(18, 223).cuboid(0f, -17f, -7.5f, 0f, 15f, 18f, new Dilation(0f))
+			.uv(28, 130).cuboid(-1f, -10f, -6f, 2f, 2f, 12f, new Dilation(0f))
+			.uv(52, 128).cuboid(-1f, -8f, -6f, 2f, 6f, 2f, new Dilation(0f))
+			.uv(44, 128).cuboid(-1f, -8f, 4f, 2f, 8f, 2f, new Dilation(0f))
+			.uv(32, 144).cuboid(-4f, -8f, -4f, 8f, 8f, 8f, new Dilation(0.55f)), ModelTransform.pivot(0f, 0f, 0f));
 		ModelPartData armet_r1 = armorHead.addChild("armet_r1", ModelPartBuilder.create().uv(49, 137).cuboid(-4.5f, 2.25f, -4.75f, 9f, 2f, 3f, new Dilation(0f)), ModelTransform.of(0f, 0f, 0f, -1.0472f, 0f, 0f));
 
 		ModelPartData armorBody = body.addChild("armorBody", ModelPartBuilder.create().uv(0, 192).cuboid(-4.5f, 9.2f, -2.5f, 9f, 7f, 5f, new Dilation(0f))
-		.uv(48, 179).cuboid(-4.5f, 9.2f, -2.5f, 9f, 7f, 5f, new Dilation(0.01f))
-		.uv(40, 160).cuboid(-5f, 0f, -2f, 10f, 4f, 4f, new Dilation(0.66f))
-		.uv(0, 160).cuboid(-4f, 0f, -2f, 8f, 12f, 4f, new Dilation(0.4f))
-		.uv(56, 203).cuboid(-4f, 0f, -2f, 8f, 12f, 4f, new Dilation(0.42f))
-		.uv(0, 204).cuboid(-4f, 0f, -2f, 8f, 8f, 4f, new Dilation(0.51f)), ModelTransform.pivot(0f, 0f, 0f));
+			.uv(40, 160).cuboid(-5f, 0f, -2f, 10f, 4f, 4f, new Dilation(0.66f))
+			.uv(0, 204).cuboid(-4f, 0f, -2f, 8, 8, 4, new Dilation(0.51f)), ModelTransform.pivot(0f, 0f, 0f));
 		ModelPartData surcoatFront = armorBody.addChild("surcoatFront", ModelPartBuilder.create().uv(0, 216).cuboid(-4f, 0.5f, 0.5f, 8f, 8f, 2f, new Dilation(0.51f)), ModelTransform.of(0f, 8.5f, -2.5f, -0.0436f, 0f, 0f));
 		ModelPartData surcoatBack = armorBody.addChild("surcoatBack", ModelPartBuilder.create().uv(20, 216).cuboid(-4f, 0.5f, -2.5f, 8f, 8f, 2f, new Dilation(0.51f)), ModelTransform.of(0f, 8.5f, 2.5f, 0.0436f, 0f, 0f));
 		ModelPartData trinketStuff = armorBody.addChild("trinketStuff", ModelPartBuilder.create().uv(0, 226).cuboid(-5f, 9f, -2.75f, 10f, 2f, 3f, new Dilation(0f))
-		.uv(0, 231).cuboid(-5f, 9f, -0.25f, 10f, 2f, 3f, new Dilation(0f)), ModelTransform.pivot(0f, 0f, 0f));
+			.uv(0, 231).cuboid(-5f, 9f, -0.25f, 10f, 2f, 3f, new Dilation(0f)), ModelTransform.pivot(0f, 0f, 0f));
 
-		ModelPartData armorRightArm = rightArm.addChild("armorRightArm", ModelPartBuilder.create().uv(24, 160).cuboid(-3f, -2f, -2f, 4f, 12f, 4f, new Dilation(0.3f))
-		.uv(60, 191).cuboid(-3f, -2f, -2f, 5f, 6f, 4f, new Dilation(0.55f))
-		.uv(40, 168).cuboid(-4.5f, -3.75f, -1f, 2f, 6f, 2f, new Dilation(0f))
-		.uv(44, 191).cuboid(-3f, -2f, -2f, 4f, 12f, 4f, new Dilation(0.41f))
-		.uv(28, 191).cuboid(-3f, -2f, -2f, 4f, 12f, 4f, new Dilation(0.5f)), ModelTransform.pivot(0f, 0f, 0f));
-		ModelPartData armorLeftArm = leftArm.addChild("armorLeftArm", ModelPartBuilder.create().uv(24, 160).mirrored().cuboid(-1f, -2f, -2f, 4f, 12f, 4f, new Dilation(0.3f)).mirrored(false)
-		.uv(60, 191).mirrored().cuboid(-2f, -2f, -2f, 5f, 6f, 4f, new Dilation(0.55f)).mirrored(false)
-		.uv(40, 168).mirrored().cuboid(2.5f, -3.75f, -1f, 2f, 6f, 2f, new Dilation(0f)).mirrored(false)
-		.uv(44, 191).mirrored().cuboid(-1f, -2f, -2f, 4f, 12f, 4f, new Dilation(0.41f)).mirrored(false)
-		.uv(28, 191).mirrored().cuboid(-1f, -2f, -2f, 4f, 12f, 4f, new Dilation(0.5f)).mirrored(false), ModelTransform.pivot(0f, 0f, 0f));
+		ModelPartData armorRightArm = rightArm.addChild("armorRightArm", ModelPartBuilder.create().uv(60, 191).cuboid(-3f, -2f, -2f, 5f, 6f, 4f, new Dilation(0.55f))
+			.uv(40, 168).cuboid(-4.5f, -3.75f, -1f, 2f, 6f, 2f, new Dilation(0f))
+			.uv(44, 191).cuboid(-3f, -2f, -2f, 4f, 12f, 4f, new Dilation(0.41f)), ModelTransform.pivot(0f, 0f, 0f));
+		ModelPartData armorLeftArm = leftArm.addChild("armorLeftArm", ModelPartBuilder.create().uv(60, 191).mirrored().cuboid(-2f, -2f, -2f, 5f, 6f, 4f, new Dilation(0.55f)).mirrored(false)
+			.uv(40, 168).mirrored().cuboid(2.5f, -3.75f, -1f, 2f, 6f, 2f, new Dilation(0f)).mirrored(false)
+			.uv(44, 191).mirrored().cuboid(-1f, -2f, -2f, 4f, 12f, 4f, new Dilation(0.41f)).mirrored(false), ModelTransform.pivot(0f, 0f, 0f));
 
 		ModelPartData armorRightLeg = rightLeg.addChild("armorRightLeg", ModelPartBuilder.create().uv(0, 176).cuboid(-1.9f, 0f, -2f, 4f, 12f, 4f, new Dilation(0.3f)), ModelTransform.pivot(0f, 0f, 0f));
 		ModelPartData rightTasset = armorRightLeg.addChild("rightTasset", ModelPartBuilder.create().uv(32, 176).cuboid(-2.55f, -1f, -2.55f, 4f, 11f, 4f, new Dilation(0f)), ModelTransform.pivot(0f, 0f, 0f));
