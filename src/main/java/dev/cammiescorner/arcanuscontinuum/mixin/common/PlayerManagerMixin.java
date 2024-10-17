8f6aeed1952f7509bc19423d227428c314bd6a67
Up <upcraftedlp@gmail.com>
2024-10-17 12:13:03 +0200

    code cleanup

--- src/main/java/dev/cammiescorner/arcanuscontinuum/mixin/common/PlayerManagerMixin.java
+++ src/main/java/dev/cammiescorner/arcanuscontinuum/mixin/common/PlayerManagerMixin.java
@@ -4,6 +4,7 @@ import com.llamalad7.mixinextras.injector.ModifyReceiver;
 import dev.cammiescorner.arcanuscontinuum.Arcanus;
 import dev.cammiescorner.arcanuscontinuum.common.blocks.MagicDoorBlock;
 import dev.cammiescorner.arcanuscontinuum.common.blocks.entities.MagicDoorBlockEntity;
+import dev.cammiescorner.arcanuscontinuum.common.registry.ArcanusBlocks;
 import dev.cammiescorner.arcanuscontinuum.common.registry.ArcanusPointsOfInterest;
 import net.minecraft.block.BlockState;
 import net.minecraft.network.message.MessageType;
@@ -41,22 +42,23 @@ public class PlayerManagerMixin {
 		if (player != null && this.registryManager.getCompositeManager().get(RegistryKeys.MESSAGE_TYPE).getKey(parameters.messageType()).map(key -> key.equals(MessageType.CHAT)).orElse(false)) {
 			ServerWorld world = player.getServerWorld();
 			PointOfInterestStorage poiStorage = world.getChunkManager().getPointOfInterestStorage();
-			Stream<BlockPos> pointOfInterest = poiStorage.getInSquare(poiTypeHolder -> poiTypeHolder.isRegistryKey(ArcanusPointsOfInterest.MAGIC_DOOR), player.getBlockPos(), 8, PointOfInterestStorage.OccupationStatus.ANY).map(PointOfInterest::getPos);
-			boolean beep = false;
 
-			for (BlockPos pos : pointOfInterest.collect(Collectors.toSet())) {
+			var beep = new boolean[1];
+
+			poiStorage.getInCircle(poiTypeHolder -> poiTypeHolder.isRegistryKey(ArcanusPointsOfInterest.MAGIC_DOOR), player.getBlockPos(), 8, PointOfInterestStorage.OccupationStatus.ANY).map(PointOfInterest::getPos).forEach(pos -> {
 				BlockState state = world.getBlockState(pos);
-
 				if (state.getBlock() instanceof MagicDoorBlock doorBlock && world.getBlockEntity(pos) instanceof MagicDoorBlockEntity door) {
-					if (chatMessage.getContent().toLowerCase(Locale.ROOT).equals(door.getPassword())) {
+					if (chatMessage.getContent().equalsIgnoreCase(door.getPassword())) {
 						doorBlock.setOpen(null, world, state, pos, true);
 						player.sendMessage(Arcanus.translate("door", "access_granted").formatted(Formatting.GRAY, Formatting.ITALIC), true);
-						beep = true;
+						beep[0] = true;
 					}
 				}
-			}
-			if (beep)
+			});
+
+			if (beep[0]) {
 				return List.of();
+			}
 		}
 		return original;
 	}
