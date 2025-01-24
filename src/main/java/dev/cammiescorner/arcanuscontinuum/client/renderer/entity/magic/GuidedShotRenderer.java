8643914ba2839df7d07de11e673d7bb649801023
CammiePone <inazuma261@gmail.com>
2025-01-24 12:19:30 -0800

    add another todo

--- src/main/java/dev/cammiescorner/arcanuscontinuum/client/renderer/entity/magic/GuidedShotRenderer.java
+++ src/main/java/dev/cammiescorner/arcanuscontinuum/client/renderer/entity/magic/GuidedShotRenderer.java
@@ -1,7 +1,9 @@
 package dev.cammiescorner.arcanuscontinuum.client.renderer.entity.magic;
 
+import com.mojang.blaze3d.vertex.PoseStack;
 import dev.cammiescorner.arcanuscontinuum.client.ArcanusClient;
 import dev.cammiescorner.arcanuscontinuum.common.entities.magic.GuidedShot;
+import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.entity.EntityRenderer;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.resources.ResourceLocation;
@@ -9,6 +11,12 @@ import net.minecraft.resources.ResourceLocation;
 public class GuidedShotRenderer extends EntityRenderer<GuidedShot> {
 	public GuidedShotRenderer(EntityRendererProvider.Context context) {
 		super(context);
+	}
+
+	@Override
+	public void render(GuidedShot entity, float entityYaw, float partialTick, PoseStack poseStack, MultiBufferSource buffer, int packedLight) {
+		// TODO create and render model
+		super.render(entity, entityYaw, partialTick, poseStack, buffer, packedLight);
 	}
 
 	@Override
