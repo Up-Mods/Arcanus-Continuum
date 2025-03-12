68b4ac08407adf379ccab1825b4eb4e5b3184197
Up <upcraftedlp@gmail.com>
2025-03-12 05:12:56 +0100

    formatting

--- src/main/java/dev/cammiescorner/arcanuscontinuum/datagen/common/ArcanusBiomeProvider.java
+++ src/main/java/dev/cammiescorner/arcanuscontinuum/datagen/common/ArcanusBiomeProvider.java
@@ -17,7 +17,13 @@ public class ArcanusBiomeProvider extends DynamicRegistryEntryProvider {
 			var placedFeatures = context.lookup(Registries.PLACED_FEATURE);
 			var worldCarvers = context.lookup(Registries.CONFIGURED_CARVER);
 
-			context.register(ArcanusBiomes.POCKET_DIMENSION, new Biome.BiomeBuilder().hasPrecipitation(false).temperature(0.5F).downfall(0.5F).specialEffects(new BiomeSpecialEffects.Builder().waterColor(0x3F76E4).waterFogColor(0x050533).fogColor(0xC0D8FF).skyColor(0x000000).build()).mobSpawnSettings(new MobSpawnSettings.Builder().build()).generationSettings(new BiomeGenerationSettings.Builder(placedFeatures, worldCarvers).build()).build());
+			context.register(ArcanusBiomes.POCKET_DIMENSION, new Biome.BiomeBuilder()
+				.hasPrecipitation(false)
+				.temperature(0.5F)
+				.downfall(0.5F)
+				.specialEffects(new BiomeSpecialEffects.Builder().waterColor(0x3F76E4).waterFogColor(0x050533).fogColor(0xC0D8FF).skyColor(0x000000).build())
+				.mobSpawnSettings(new MobSpawnSettings.Builder().build())
+				.generationSettings(new BiomeGenerationSettings.Builder(placedFeatures, worldCarvers).build()).build());
 		});
 	}
 }
