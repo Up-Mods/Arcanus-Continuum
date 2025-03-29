2589d8ebea4f84696e84a29b452ec99b1ec6e5b0
CammiePone <inazuma261@gmail.com>
2025-03-29 14:02:53 -0700

    fix lang thing

--- src/main/java/dev/cammiescorner/arcanuscontinuum/common/items/SpellBookItem.java
+++ src/main/java/dev/cammiescorner/arcanuscontinuum/common/items/SpellBookItem.java
@@ -51,7 +51,7 @@ public class SpellBookItem extends Item {
 		// TODO make ALL of it translatable
 		tooltip.add(Component.literal(spell.getName()).withStyle(ChatFormatting.GOLD));
 		tooltip.add(Component.translatable("spell_book.arcanuscontinuum.weight").append(": ").withStyle(ChatFormatting.GREEN)
-			.append(Component.translatable("spell_book.arcanuscontinuum.weight", spell.getWeight().toString().toLowerCase(Locale.ROOT)).withStyle(ChatFormatting.GRAY)));
+			.append(Component.translatable("spell_book.arcanuscontinuum.weight." + spell.getWeight().toString().toLowerCase(Locale.ROOT)).withStyle(ChatFormatting.GRAY)));
 		tooltip.add(Component.translatable("spell_book.arcanuscontinuum.mana_cost").append(": ").withStyle(ChatFormatting.BLUE)
 			.append(Component.literal(manaCost).withStyle(ChatFormatting.GRAY)));
 		tooltip.add(Component.translatable("spell_book.arcanuscontinuum.cool_down").append(": ").withStyle(ChatFormatting.RED)
