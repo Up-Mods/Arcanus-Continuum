3d5a6f1542770b2ddbe4bf3a940427b5d495aec6
CammiePone <inazuma261@gmail.com>
2025-06-11 21:00:08 -0700

    RAAAAAAAAAHHHHHH!!!!!!!

--- src/main/java/dev/cammiescorner/arcanus/common/items/SpellBookItem.java
+++ src/main/java/dev/cammiescorner/arcanus/common/items/SpellBookItem.java
@@ -5,21 +5,15 @@ import dev.cammiescorner.arcanus.api.spells.Spell;
 import dev.cammiescorner.arcanus.common.registry.ArcanusDataComponents;
 import dev.cammiescorner.arcanus.common.screens.providers.SpellBookMenuProvider;
 import net.minecraft.ChatFormatting;
-import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.world.InteractionHand;
-import net.minecraft.world.InteractionResult;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
-import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.LecternBlock;
-import net.minecraft.world.level.block.state.BlockState;
 
 import java.util.List;
 import java.util.Locale;
@@ -53,18 +47,6 @@ public class SpellBookItem extends Item {
 			.append(Component.literal(coolDown).append(Component.translatable("spell_book.arcanus.seconds")).withStyle(ChatFormatting.GRAY)));
 
 		super.appendHoverText(stack, context, tooltipComponents, tooltipFlag);
-	}
-
-	@Override
-	public InteractionResult useOn(UseOnContext context) {
-		Level world = context.getLevel();
-		BlockPos pos = context.getClickedPos();
-		BlockState state = world.getBlockState(pos);
-
-		if(state.is(Blocks.LECTERN))
-			return LecternBlock.tryPlaceBook(context.getPlayer(), world, pos, state, context.getItemInHand()) ? InteractionResult.sidedSuccess(world.isClientSide) : InteractionResult.PASS;
-
-		return InteractionResult.PASS;
 	}
 
 	@Override
--- src/main/java/dev/cammiescorner/arcanus/common/registry/ArcanusMenus.java
+++ src/main/java/dev/cammiescorner/arcanus/common/registry/ArcanusMenus.java
@@ -9,12 +9,11 @@ import dev.upcraft.sparkweave.api.registry.RegistrySupplier;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.world.flag.FeatureFlags;
 import net.minecraft.world.inventory.MenuType;
-import net.minecraft.world.item.ItemStack;
 
 public class ArcanusMenus {
 	public static final RegistryHandler<MenuType<?>> MENUS = RegistryHandler.create(Registries.MENU, Arcanus.MOD_ID);
 
 	public static final RegistrySupplier<MenuType<SpellcraftMenu>> SPELLCRAFT_MENU = MENUS.register("spellcraft_menu", () -> new MenuType<>(SpellcraftMenu::new, FeatureFlags.VANILLA_SET));
-	public static final RegistrySupplier<MenuType<SpellBookMenu>> SPELL_BOOK_MENU = MENUS.register("spell_book_menu", () -> new MenuType<>((i, inventory) -> new SpellBookMenu(i, ItemStack.EMPTY), FeatureFlags.VANILLA_SET));
+	public static final RegistrySupplier<MenuType<SpellBookMenu>> SPELL_BOOK_MENU = MENUS.register("spell_book_menu", () -> new MenuType<>((i, inventory) -> new SpellBookMenu(i, ArcanusItems.SPELL_BOOK.get().getDefaultInstance()), FeatureFlags.VANILLA_SET));
 	public static final RegistrySupplier<MenuType<ArcaneWorkbenchMenu>> ARCANE_WORKBENCH_MENU = MENUS.register("arcane_workbench_menu", () -> new MenuType<>(ArcaneWorkbenchMenu::new, FeatureFlags.VANILLA_SET));
 }
