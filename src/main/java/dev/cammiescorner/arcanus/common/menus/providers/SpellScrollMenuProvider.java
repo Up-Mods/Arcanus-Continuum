62047ea09b52e458ced2bfc1a0ec7c7b96c38d5d
Up <upcraftedlp@gmail.com>
2025-06-27 08:39:28 +0200

    switch spell scroll menu to extended provider

--- src/main/java/dev/cammiescorner/arcanus/common/menus/providers/SpellScrollMenuProvider.java
+++ src/main/java/dev/cammiescorner/arcanus/common/menus/providers/SpellScrollMenuProvider.java
@@ -3,7 +3,10 @@ package dev.cammiescorner.arcanus.common.menus.providers;
 import commonnetwork.api.Network;
 import dev.cammiescorner.arcanus.common.networking.clientbound.ClientboundUpdateSpellScrollScreenPacket;
 import dev.cammiescorner.arcanus.common.menus.SpellScrollMenu;
+import net.fabricmc.fabric.api.screenhandler.v1.ExtendedScreenHandlerFactory;
+import net.minecraft.network.RegistryFriendlyByteBuf;
 import net.minecraft.network.chat.Component;
+import net.minecraft.network.codec.StreamCodec;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.MenuProvider;
 import net.minecraft.world.entity.player.Inventory;
@@ -12,7 +15,7 @@ import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.item.ItemStack;
 import org.jetbrains.annotations.Nullable;
 
-public class SpellScrollMenuProvider implements MenuProvider {
+public class SpellScrollMenuProvider implements ExtendedScreenHandlerFactory<SpellScrollMenuProvider.MenuData> {
 	private final ItemStack stack;
 
 	public SpellScrollMenuProvider(ItemStack stack) {
@@ -26,9 +29,15 @@ public class SpellScrollMenuProvider implements MenuProvider {
 
 	@Override
 	public @Nullable AbstractContainerMenu createMenu(int i, Inventory inventory, Player player) {
-		if(player instanceof ServerPlayer serverPlayer)
-			Network.getNetworkHandler().sendToClient(new ClientboundUpdateSpellScrollScreenPacket(stack), serverPlayer);
-
 		return new SpellScrollMenu(i, stack);
+	}
+
+	@Override
+	public MenuData getScreenOpeningData(ServerPlayer player) {
+		return new MenuData(stack);
+	}
+
+	public record MenuData(ItemStack stack) {
+		public static final StreamCodec<RegistryFriendlyByteBuf, SpellBookMenuProvider.MenuData> STREAM_CODEC = ItemStack.STREAM_CODEC.map(SpellBookMenuProvider.MenuData::new, SpellBookMenuProvider.MenuData::stack);
 	}
 }
--- src/main/java/dev/cammiescorner/arcanus/common/registry/ArcanusMenus.java
+++ src/main/java/dev/cammiescorner/arcanus/common/registry/ArcanusMenus.java
@@ -6,6 +6,7 @@ import dev.cammiescorner.arcanus.common.menus.SpellBookMenu;
 import dev.cammiescorner.arcanus.common.menus.SpellScrollMenu;
 import dev.cammiescorner.arcanus.common.menus.SpellcraftMenu;
 import dev.cammiescorner.arcanus.common.menus.providers.SpellBookMenuProvider;
+import dev.cammiescorner.arcanus.common.menus.providers.SpellScrollMenuProvider;
 import dev.upcraft.sparkweave.api.registry.RegistryHandler;
 import dev.upcraft.sparkweave.api.registry.RegistrySupplier;
 import net.fabricmc.fabric.api.screenhandler.v1.ExtendedScreenHandlerType;
@@ -18,7 +19,7 @@ public class ArcanusMenus {
 	public static final RegistryHandler<MenuType<?>> MENUS = RegistryHandler.create(Registries.MENU, Arcanus.MOD_ID);
 
 	public static final RegistrySupplier<MenuType<SpellcraftMenu>> SPELLCRAFT_MENU = MENUS.register("spellcraft_menu", () -> new MenuType<>(SpellcraftMenu::new, FeatureFlags.VANILLA_SET));
-	public static final RegistrySupplier<MenuType<SpellScrollMenu>> SPELL_SCROLL_MENU = MENUS.register("spell_scroll_menu", () -> new MenuType<>((i, inventory) -> new SpellScrollMenu(i, ArcanusItems.SPELL_SCROLL.get().getDefaultInstance()), FeatureFlags.VANILLA_SET));
+	public static final RegistrySupplier<MenuType<SpellScrollMenu>> SPELL_SCROLL_MENU = MENUS.register("spell_scroll_menu", () -> new ExtendedScreenHandlerType<>((syncId, inventory, menuData) -> new SpellScrollMenu(syncId, menuData.stack()), SpellScrollMenuProvider.MenuData.STREAM_CODEC));
 	public static final RegistrySupplier<MenuType<SpellBookMenu>> SPELL_BOOK_MENU = MENUS.register("spell_book_menu", () -> new ExtendedScreenHandlerType<>((syncId, inventory, menuData) -> new SpellBookMenu(syncId, inventory, menuData.stack()), SpellBookMenuProvider.MenuData.STREAM_CODEC));
 	public static final RegistrySupplier<MenuType<ArcaneWorkbenchMenu>> ARCANE_WORKBENCH_MENU = MENUS.register("arcane_workbench_menu", () -> new MenuType<>(ArcaneWorkbenchMenu::new, FeatureFlags.VANILLA_SET));
 }
