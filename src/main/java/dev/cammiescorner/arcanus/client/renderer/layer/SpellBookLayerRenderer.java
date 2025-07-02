67ef1bd5159e5218ad6ab65b92795f8210a3b7a5
CammiePone <inazuma261@gmail.com>
2025-07-02 12:26:47 -0700

    oop forgot to make this check and todo

--- src/main/java/dev/cammiescorner/arcanus/client/renderer/layer/SpellBookLayerRenderer.java
+++ src/main/java/dev/cammiescorner/arcanus/client/renderer/layer/SpellBookLayerRenderer.java
@@ -3,6 +3,7 @@ package dev.cammiescorner.arcanus.client.renderer.layer;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.math.Axis;
 import dev.cammiescorner.arcanus.Arcanus;
+import dev.cammiescorner.arcanus.common.registry.ArcanusItems;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.model.EntityModel;
 import net.minecraft.client.renderer.MultiBufferSource;
@@ -24,9 +25,15 @@ public class SpellBookLayerRenderer<T extends LivingEntity, M extends EntityMode
 	@Override
 	public void render(PoseStack poseStack, MultiBufferSource bufferSource, int packedLight, T livingEntity, float limbSwing, float limbSwingAmount, float partialTick, float ageInTicks, float netHeadYaw, float headPitch) {
 		ItemStack stack = Arcanus.getActiveSpellBook(livingEntity);
-		poseStack.pushPose();
 
-		if(!stack.isEmpty()) {
+		if(stack.is(ArcanusItems.BOOK_POUCH.get())) {
+			// TODO add render if stack is BookPouchItem
+			poseStack.pushPose();
+			poseStack.popPose();
+		}
+
+		if(stack.is(ArcanusItems.SPELL_BOOK.get())) {
+			poseStack.pushPose();
 			poseStack.translate(client.options.mainHand().get() == HumanoidArm.RIGHT ? 0.275 : -0.275, 1, 0.05);
 			poseStack.mulPose(Axis.YP.rotationDegrees(-90));
 			poseStack.mulPose(Axis.ZP.rotationDegrees(150));
@@ -41,8 +48,8 @@ public class SpellBookLayerRenderer<T extends LivingEntity, M extends EntityMode
 				livingEntity.level(),
 				0
 			);
-		}
 
-		poseStack.popPose();
+			poseStack.popPose();
+		}
 	}
 }
