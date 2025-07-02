9c615c19d3462e78391529f9f46f6bade2cf08e2
CammiePone <inazuma261@gmail.com>
2025-07-02 12:50:42 -0700

    chalk needs solid block to be placed

--- src/main/java/dev/cammiescorner/arcanus/common/block/ChalkBlock.java
+++ src/main/java/dev/cammiescorner/arcanus/common/block/ChalkBlock.java
@@ -9,10 +9,8 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.HorizontalDirectionalBlock;
-import net.minecraft.world.level.block.Mirror;
-import net.minecraft.world.level.block.Rotation;
+import net.minecraft.world.level.LevelReader;
+import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.DirectionProperty;
@@ -45,6 +43,14 @@ public class ChalkBlock extends Block implements BlockItemProvider {
 	@Override
 	protected BlockState mirror(BlockState state, Mirror mirror) {
 		return state.rotate(mirror.getRotation(state.getValue(FACING)));
+	}
+
+	@Override
+	protected boolean canSurvive(BlockState state, LevelReader level, BlockPos pos) {
+		BlockPos blockPos = pos.below();
+		BlockState blockState = level.getBlockState(blockPos);
+
+		return blockState.isFaceSturdy(level, blockPos, Direction.UP);
 	}
 
 	@Override
