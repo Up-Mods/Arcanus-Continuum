ed6104162ebd897c60a6eb95e65f5bf2fad0cf9e
CammiePone <inazuma261@gmail.com>
2025-07-02 01:17:19 -0700

    almost forgor to do translation 💀

--- src/main/java/dev/cammiescorner/arcanus/datagen/client/ArcanusEnglishLanguageProvider.java
+++ src/main/java/dev/cammiescorner/arcanus/datagen/client/ArcanusEnglishLanguageProvider.java
@@ -109,6 +109,7 @@ public class ArcanusEnglishLanguageProvider extends SparkweaveLanguageProvider {
 
 		builder.block(ArcanusBlocks.ARCANE_WORKBENCH, "Arcane Workbench");
 		builder.block(ArcanusBlocks.PEDESTAL, "Pedestal");
+		builder.block(ArcanusBlocks.CHALK, "Chalk");
 		builder.block(ArcanusBlocks.DUMMY_BOOKSHELF, "[DEVELOPER ONLY] Dummy Chiseled Bookshelf");
 		builder.block(ArcanusBlocks.MAGIC_BLOCK, "Magic Block");
 		builder.block(ArcanusBlocks.MAGIC_DOOR, "Magic Door");
