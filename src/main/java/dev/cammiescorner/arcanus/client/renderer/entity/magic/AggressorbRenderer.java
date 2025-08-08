d680972d7cdc6312708b90397d1b75a5c8aeb7bd
CammiePone <inazuma261@gmail.com>
2025-08-08 04:51:06 -0700

    hooray it works with the icosohedron now

--- src/main/java/dev/cammiescorner/arcanus/client/renderer/entity/magic/AggressorbRenderer.java
+++ src/main/java/dev/cammiescorner/arcanus/client/renderer/entity/magic/AggressorbRenderer.java
@@ -2,9 +2,9 @@ package dev.cammiescorner.arcanus.client.renderer.entity.magic;
 
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
+import com.mojang.math.Axis;
 import dev.cammiescorner.arcanus.Arcanus;
 import dev.cammiescorner.arcanus.client.ArcanusClient;
-import dev.cammiescorner.arcanus.client.model.entity.magic.AggressorbModel;
 import dev.cammiescorner.arcanus.common.entity.magic.Aggressorb;
 import dev.cammiescorner.arcanus.common.util.ArcanusHelper;
 import dev.upcraft.sparkweave.api.color.Color;
@@ -15,28 +15,24 @@ import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.resources.ResourceLocation;
 
 public class AggressorbRenderer extends EntityRenderer<Aggressorb> {
-	private static final ResourceLocation TEXTURE = Arcanus.id("textures/entity/magic/lob.png");
-	private final AggressorbModel model;
+	private static final ResourceLocation TEXTURE = Arcanus.id("textures/block/magic_block.png");
 
 	public AggressorbRenderer(EntityRendererProvider.Context context) {
 		super(context);
-		model = new AggressorbModel(context.getModelSet().bakeLayer(AggressorbModel.MODEL_LAYER));
 	}
 
 	@Override
 	public void render(Aggressorb entity, float yaw, float tickDelta, PoseStack matrices, MultiBufferSource vertices, int light) {
-		VertexConsumer consumer = vertices.getBuffer(ArcanusClient.getMagicCircles(getTextureLocation(entity)));
+		VertexConsumer consumer = vertices.getBuffer(ArcanusClient.getMagicCirclesTri(getTextureLocation(entity))); //vertices.getBuffer(ArcanusClient.getMagicCircles(getTextureLocation(entity)));
 		Color color = ArcanusHelper.getMagicColor(entity);
 
 		matrices.pushPose();
 		matrices.translate(0, 0.2, 0);
-		model.cube1.xRot = (entity.tickCount + tickDelta) * 0.1f;
-		model.cube1.yRot = (entity.tickCount + tickDelta) * 0.1f;
-		model.cube2.yRot = -(entity.tickCount + tickDelta) * 0.125f;
-		model.cube2.zRot = -(entity.tickCount + tickDelta) * 0.125f;
-		model.cube3.zRot = (entity.tickCount + tickDelta) * 0.15f;
-		model.cube3.xRot = (entity.tickCount + tickDelta) * 0.15f;
-		model.renderToBuffer(matrices, consumer, light, OverlayTexture.NO_OVERLAY, color.asIntARGB());
+		matrices.mulPose(Axis.XN.rotationDegrees((entity.tickCount + tickDelta) * 2));
+		matrices.mulPose(Axis.YP.rotationDegrees((entity.tickCount + tickDelta) * 2));
+		matrices.mulPose(Axis.ZP.rotationDegrees((entity.tickCount + tickDelta) * 2));
+		matrices.scale(0.25f, 0.25f, 0.25f);
+		ManaShieldRenderer.drawIcosahedron(matrices, consumer, color, 1f, light, OverlayTexture.NO_OVERLAY);
 		matrices.popPose();
 	}
 
