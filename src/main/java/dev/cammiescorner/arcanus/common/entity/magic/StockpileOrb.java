edcf82d62c9a1f18a191a6363a1b22b8dab2b5e6
CammiePone <inazuma261@gmail.com>
2025-08-12 09:43:50 -0700

    remove todo

--- src/main/java/dev/cammiescorner/arcanus/common/entity/magic/StockpileOrb.java
+++ src/main/java/dev/cammiescorner/arcanus/common/entity/magic/StockpileOrb.java
@@ -77,7 +77,6 @@ public class StockpileOrb extends ThrowableProjectile implements Targetable {
 		}
 
 		if(isBoundToTarget()) {
-			// TODO for some reason these are moving down and i dont know why
 			int orbCount = ArcanusComponents.stockpileOrbCount(getTarget());
 			int orbIndex = ArcanusComponents.stockpileOrbIndex(getTarget(), this) + 1;
 			double angle = Math.toRadians(360d / orbCount * orbIndex);
