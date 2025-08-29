d17d790fe19b952c330329e56b39c633848498b1
CammiePone <inazuma261@gmail.com>
2025-08-29 01:02:39 -0700

    finalize jar size

--- src/main/java/dev/cammiescorner/arcanus/common/block/JarBlock.java
+++ src/main/java/dev/cammiescorner/arcanus/common/block/JarBlock.java
@@ -28,14 +28,14 @@ import org.jetbrains.annotations.Nullable;
 public class JarBlock extends Block implements BlockItemProvider, EntityBlock {
 	private static final VoxelShape SHAPE = Shapes.or(
 		Shapes.box(0.1875,  0,      0.1875,  0.8125,  0.0625, 0.8125), // bottom
-		Shapes.box(0.1875,  0.0625, 0.1875,  0.8125,  0.8125, 0.25),   // front
-		Shapes.box(0.1875,  0.0625, 0.1875,  0.25,    0.8125, 0.8125), // right side
-		Shapes.box(0.75,    0.0625, 0.1875,  0.8125,  0.8125, 0.8125), // left side
-		Shapes.box(0.1875,  0.0625, 0.75,    0.8125,  0.8125, 0.8125), // back
-		Shapes.box(0.1875,  0.8125, 0.1875,  0.8125,  0.875,  0.8125), // top
-		Shapes.box(0.28125, 0.875,  0.28125, 0.71875, 1,      0.71875) // lid
+		Shapes.box(0.1875,  0.0625, 0.1875,  0.8125,  0.6875, 0.25),   // front
+		Shapes.box(0.1875,  0.0625, 0.1875,  0.25,    0.6875, 0.8125), // right side
+		Shapes.box(0.75,    0.0625, 0.1875,  0.8125,  0.6875, 0.8125), // left side
+		Shapes.box(0.1875,  0.0625, 0.75,    0.8125,  0.6875, 0.8125), // back
+		Shapes.box(0.1875,  0.6875, 0.1875,  0.8125,  0.75,   0.8125), // top
+		Shapes.box(0.28125, 0.75,   0.28125, 0.71875, 0.875,  0.71875) // lid
 	);
-	private static final VoxelShape INSIDE = Shapes.box(0.25, 0.0625, 0.25, 0.75, 0.8125, 0.75);
+	private static final VoxelShape INSIDE = Shapes.box(0.25, 0.0625, 0.25, 0.75, 0.6875, 0.75);
 	public static final DirectionProperty FACING = HorizontalDirectionalBlock.FACING;
 	public static final IntegerProperty LEVEL = IntegerProperty.create("level", 0, 8);
 
